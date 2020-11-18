2d68f4fa393515173c68820d47575204b433c7f7
Bruce Berrios <bberr022@fiu.edu>
2020-11-17 21:28:53 -0500

    Add app

---
+++ app.js
@@ -0,0 +1,33 @@
+const joi = require('joi');
+const express = require('express');
+
+const logger = require('./logger.js');
+const courseRepo = require('./courseRepo.js');
+
+courseRepo.init();
+const app = express();
+
+const querySchema = joi.object({
+    subject: joi.string().optional(),
+    code: joi.string().optional(),
+    units: joi.string().optional(),
+    isLab: joi.optional(),
+});
+
+app.use(logger());
+
+app.get('/api', (req, res) => {
+    const { value, error } = querySchema.validate(req.query);
+    const results = error ? [] : courseRepo.queryBy(value);
+    return res.status(200).json({
+        total: results.length,
+        results,
+        ...(error && { error: error.details[0].message })
+    });
+});
+
+app.get('*', (_req, res) => {
+    res.status(200).send('TODO: API Documentation.');
+});
+
+app.listen(8000, () => console.log('Server started at port 8080!'));
