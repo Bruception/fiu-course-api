00fe9ba1b1318013ec3cd9b7c4f34e3bda628913
Bruce Berrios <bberr022@fiu.edu>
2020-11-18 20:11:54 -0500

    Update default port number

--- src/app.js
+++ src/app.js
@@ -28,5 +28,5 @@ app.get('*', (_req, res) => {
     res.status(200).send('TODO: API Documentation.');
 });
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server started at port ${PORT}!`));
