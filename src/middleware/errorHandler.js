365fb1b561da815a0b1db6ba1a9f791a9dad3a8b
Bruce Berrios <bberr022@fiu.edu>
2020-11-29 18:08:41 -0500

    Log error stack message

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -2,6 +2,7 @@ const DEFAULT_ERROR_MESSAGE = 'Oops! Something went wrong.'
 const DEFAULT_ERROR_STATUS_CODE = 500;
 
 module.exports = (error, _req, res, _next) => {
+    console.log(error.stack);
     res.status(error.statusCode || DEFAULT_ERROR_STATUS_CODE).json({
         error: error.message || DEFAULT_ERROR_MESSAGE,
     });
