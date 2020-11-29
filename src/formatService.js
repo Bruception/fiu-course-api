3a44cbdf1f96c97248177ed00cdd01a16bd7388d
Bruce Berrios <bberr022@fiu.edu>
2020-11-29 18:09:03 -0500

    Add YAML and XML formatting

--- src/formatService.js
+++ src/formatService.js
@@ -1,12 +1,42 @@
-const SUPPORTED_FORMATS = ['json', 'text', 'xml'];
+const xml = require('xml');
+const yaml = require('yaml');
+
+const SUPPORTED_FORMATS = ['json', 'text', 'xml', 'yaml'];
+
+const getDataShape = (data) => {
+    return {
+        total: data.length,
+        results: data,
+    };
+}
+
+const getXMLParsableObject = (data) => {
+    const mappedResults = data.results.map((result) => {
+        const mappedProperties = Object.keys(result).map((property) => {
+            return {
+                [property]: result[property],
+            };
+        });
+        return {
+            result: mappedProperties,
+        };
+    });
+    const xmlParseableObject = {
+        root: [
+            {
+                total: data.total,
+            },
+            {
+                results: mappedResults,
+            },
+        ],
+    };
+    return xmlParseableObject;
+}
 
 const formatters = {
     'json': (data) => {
-        const dataShape = {
-            total: data.length,
-            results: data,
-        };
-        return JSON.stringify(dataShape);
+        return JSON.stringify(getDataShape(data));
     },
     'text': (data) => {
         const buffer = [`results: ${data.length}`];
@@ -20,14 +50,18 @@ const formatters = {
         return buffer.join('\n');
     },
     'xml': (data) => {
-        return data;
+        const xmlParseableObject = getXMLParsableObject(getDataShape(data));
+        return xml(xmlParseableObject, true);
+    },
+    'yaml': (data) => {
+        return yaml.stringify(getDataShape(data));
     },
 }
 
 module.exports = {
     SUPPORTED_FORMATS,
     format(data, type) {
-        const targetFormatter = formatters[type || 'json'];
+        const targetFormatter = formatters[type] || formatters.json;
         return targetFormatter(data);
     }
 };
