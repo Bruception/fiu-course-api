3948056ef5fd338426ba19ea57d0c3e167c6ef89
Bruce Berrios <bberr022@fiu.edu>
2020-11-29 16:51:39 -0500

    Update error handling middleware

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,8 @@
+const DEFAULT_ERROR_MESSAGE = 'Oops! Something went wrong.'
+const DEFAULT_ERROR_STATUS_CODE = 500;
 
 module.exports = (error, _req, res, _next) => {
-    console.log(error.stack);
-    res.status(500).json({
-        error: 'Oops something went wrong!',
+    res.status(error.statusCode || DEFAULT_ERROR_STATUS_CODE).json({
+        error: error.message || DEFAULT_ERROR_MESSAGE,
     });
 };
