15cc7aa45cdd62280a4c3bb303e8594986a4d512
Bruce Berrios <bberr022@fiu.edu>
2020-11-29 18:59:07 -0500

    Update and add errorHandler tests

--- src/test/middleware.test.js
+++ src/test/middleware.test.js
@@ -1,28 +1,47 @@
 const { errorHandler } = require('../middleware');
 
+const getMockResponse = () => {
+    return {
+        statusCode: 0,
+        body: {},
+        status(code) {
+            this.statusCode = code;
+            return this;
+        },
+        json(body) {
+            this.body = body;
+            return this;
+        },
+    };
+}
+
 describe('middleware: Test middleware functionality.', () => {
-    test('middleware.errorHandler: Error handler correctly sets response status and body.', () => {
+    test('middleware.errorHandler: Error handler correctly sets default response status and body.', () => {
         const mockError = {
             stack: 'Mock error stack trace.',
         }
-        const mockResponse = {
-            statusCode: 0,
-            body: {},
-            status(code) {
-                this.statusCode = code;
-                return this;
-            },
-            json(body) {
-                this.body = body;
-                return this;
-            },
-        }
+        const mockResponse = getMockResponse();
         const responseStatusSpy = jest.spyOn(mockResponse, 'status');
         const responseBodySpy = jest.spyOn(mockResponse, 'json');
         errorHandler(mockError, {}, mockResponse);
         expect(responseStatusSpy).toHaveBeenCalledTimes(1);
         expect(responseBodySpy).toHaveBeenCalledTimes(1);
         expect(mockResponse.statusCode).toBe(500);
-        expect(mockResponse.body.error).toBe('Oops something went wrong!');
+        expect(mockResponse.body.error).toBe('Oops! Something went wrong.');
+    });
+    test('middleware.errorHandler: Error handler correctly sets specified response status and body.', () => {
+        const mockError = {
+            stack: 'Mock error stack trace.',
+            statusCode: 404,
+            message: 'Error!',
+        }
+        const mockResponse = getMockResponse();
+        const responseStatusSpy = jest.spyOn(mockResponse, 'status');
+        const responseBodySpy = jest.spyOn(mockResponse, 'json');
+        errorHandler(mockError, {}, mockResponse);
+        expect(responseStatusSpy).toHaveBeenCalledTimes(1);
+        expect(responseBodySpy).toHaveBeenCalledTimes(1);
+        expect(mockResponse.statusCode).toBe(404);
+        expect(mockResponse.body.error).toBe('Error!');
     });
 });
