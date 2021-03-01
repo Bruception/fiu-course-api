318e12674ec14093517ad1ea13ba6380a8564116
Bruce Berrios <bberr022@fiu.edu>
2021-03-01 02:26:25 -0500

    Refactor course data scraper

--- src/data/courseDataUpdate.py
+++ src/data/courseDataUpdate.py
@@ -1,110 +1,73 @@
-import json
-import math
-import time
-import requests
-import threading
-import concurrent.futures
+import json, math, time, requests
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 SEP = '*' * 64
 BASE_URL = 'https://m.fiu.edu/catalog/'
-URL = f'{BASE_URL}index.php?action=subjectList&letter='
+SUBJECT_URL = f'{BASE_URL}index.php?action=subjectList'
 
-class CourseEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if (isinstance(obj, Course)):
-            return {
-                'subject': obj.subject,
-                'code': obj.code,
-                'name': obj.name,
-                'units': obj.units,
-                'description': obj.description,
-            }
-        return json.JSONEncoder.default(self, obj)
+def getParsedCourse(soup, href):
+    courseData = soup.find('div', id='content')
+    subject, code = courseData.div.h1.text.split()
+    courseULS = courseData.find_all('ul')
+    description = courseULS[1].li.find('p')
+    return {
+        'subject': subject,
+        'code': code,
+        'name': courseData.div.h2.text,
+        'units': courseULS[0].find('p', {'class': 'data'}).text,
+        'description': description.text if description else 'N/A',
+    }
 
-class Course:
-    def __init__(self, data):
-        self.subject, self.code = data.div.h1.text.split()
-        self.name = data.div.h2.text
-        courseULS = data.find_all('ul')
-        self.units = courseULS[0].find('p', {'class': 'data'}).text
-        description = courseULS[1].li.find('p')
-        self.description = description.text if description else 'N/A'        
+def getParsedSubject(soup, href):
+    courseURLS = soup.find('fieldset').ul.find_all('a')
+    return (href[-3:], courseURLS)
 
-def getCourses(courses):
-    def getCourseInfo(course):
-        courseHref = course.get('href')
-        courseResponse = requests.get(f'{BASE_URL}{courseHref}')
-        courseSoup = BeautifulSoup(courseResponse.text, 'html.parser')
-        courseData = courseSoup.find('div', id='content')
-        parsedCourse = Course(courseData)
-        return parsedCourse
-    courseData = []
-    threads = min(30, len(courses))
-    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-        future_to_url = {executor.submit(getCourseInfo, course): course for course in courses}
-        for future in concurrent.futures.as_completed(future_to_url):
-            course = future_to_url[future]
+def appendCourseData(course, courses):
+    print('Parsed', course['name'], '...')
+    courses.append(course)
+
+def extendCourseURLS(subject, courseURLS):
+    subjectPrefix, urls = subject
+    print('Fetched courses for subject', subjectPrefix, '...')
+    courseURLS.extend(urls)
+
+def parseData(data, parseFunction, completeFunction):
+    parsedData = []
+    def getParsedData(data):
+        href = data.get('href')
+        response = requests.get(f'{BASE_URL}{href}')
+        soup = BeautifulSoup(response.text, 'html.parser')
+        time.sleep(0.1)
+        return parseFunction(soup, href)
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        futureToData = {executor.submit(getParsedData, obj): obj for obj in data}
+        for future in as_completed(futureToData):
+            dataObject = futureToData[future]
             try:
                 data = future.result()
-                print(f'Parsed "{data.name}" ...')
-                courseData.append(data)
-            except Exception as exc:
-                print(exc)
-    return courseData
+                completeFunction(data, parsedData)
+            except Exception as exception:
+                print(exception)
+    return parsedData
 
-class CourseParser:
-    def __init__(self, letter):
-        self.letter = letter
-        self.courses = []
-        self.thread = threading.Thread(target=self.parse)
-        self.thread.start()
-
-    def parse(self):
-        requestURL = f'{URL}{self.letter}'
-        response = requests.get(requestURL)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        listOfSubjects = soup.find('fieldset')
-        for subject in listOfSubjects.ul.find_all('a'):
-            subjectHref = subject.get('href')
-            subjectResponse = requests.get(f'{BASE_URL}{subjectHref}')
-            subjectSoup = BeautifulSoup(subjectResponse.text, 'html.parser')
-            listOfCourses = subjectSoup.find('fieldset').ul
-            self.courses.extend(getCourses(listOfCourses.find_all('a')))
-
-    def join(self):
-        self.thread.join()
-
-def parseCourses(min, max):
-    courses = []
-    courseParsers = []
-    for letter in range(ord(min), ord(max) + 1):
-        l = chr(letter)
-        print(f'Starting course parser for letter {l} ...')
-        courseParsers.append(CourseParser(l))
-    for courseParser in courseParsers:
-        courseParser.join()
-        courses.extend(courseParser.courses)
-    output = open('data.json', 'w')
-    output.write('{\n\t\"data\": [\n')
+def writeCoursesToJSON():
+    response = requests.get(SUBJECT_URL)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    subjectURLS = soup.find('fieldset').ul.find_all('a')
+    courseURLS = parseData(subjectURLS, getParsedSubject, extendCourseURLS)
+    parsedCourses = parseData(courseURLS, getParsedCourse, appendCourseData)
     print(f'{SEP}\nWriting course data to data.json ...\n{SEP}')
-    isFirst = True
-    for course in courses:
-        if (not isFirst):
-            output.write(',\n')
-        output.write('\t\t')
-        json.dump(course, output, cls=CourseEncoder)
-        isFirst = False
-    output.write("\n\t]\n}")
-    output.close()
-    return courses
+    with open('data.json', 'w') as output:
+        json.dump({'data': parsedCourses}, output, default=lambda o: o.__dict__, indent=4)
+    return parsedCourses
 
 def truncate(number, digits) -> float:
     stepper = 10.0 ** digits
     return math.trunc(stepper * number) / stepper
 
 start = time.time()
-courses = parseCourses('A', 'Z')
+courses = writeCoursesToJSON()
 end = time.time()
 
 print(f'Finished parsing {len(courses)} courses in {truncate(end - start, 3)} seconds!')
