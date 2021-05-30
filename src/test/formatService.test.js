3fa1982c9f9f2d1985f69bcd56303b6b4923df70
Bruce Berrios <bberr022@fiu.edu>
2021-05-30 17:09:19 -0400

    Update test

--- src/test/formatService.test.js
+++ src/test/formatService.test.js
@@ -7,7 +7,6 @@ describe('formatService: Testing the formatService module.', () => {
     test('formatService.format: Correctly serializes object to JSON when no format is specified.', () => {
         const courses = courseDataStore.queryBy({
             subject: 'COP',
-            isLab: '',
         });
         const { formattedData, contentType } = formatService.format(courses);
         expect(JSON.parse(formattedData).results).toStrictEqual(courses);
@@ -16,7 +15,6 @@ describe('formatService: Testing the formatService module.', () => {
     test('formatService.format: Correctly serializes object to JSON.', () => {
         const courses = courseDataStore.queryBy({
             subject: 'COP',
-            isLab: '',
             format: 'json',
         });
         const { formattedData, contentType } = formatService.format(courses, 'json');
@@ -26,7 +24,6 @@ describe('formatService: Testing the formatService module.', () => {
     test('formatService.format: Correctly serializes object to YAML.', () => {
         const courses = courseDataStore.queryBy({
             subject: 'COP',
-            isLab: '',
         });
         const { formattedData, contentType } = formatService.format(courses, 'yaml');
         expect(yaml.parse(formattedData).results).toStrictEqual(courses);
@@ -54,12 +51,12 @@ describe('formatService: Testing the formatService module.', () => {
     test('formatService.format: Correctly serializes object to text.', () => {
         const courses = courseDataStore.queryBy({
             subject: 'COP',
-            isLab: '',
+            code: '2210',
         });
         const { formattedData, contentType } = formatService.format(courses, 'text');
         const formatStrings = formattedData.split('\n');
         expect(formatStrings[0]).toStrictEqual('total: 1');
-        expect(formatStrings[1]).toStrictEqual('subject: COP, code: 2210L, name: Lab for Programming I, units: 0.00, description: Lab component for Computer Programming I Lecture. Corequisite: COP 2210');
+        expect(formatStrings[1]).toStrictEqual('subject: COP, code: 2210, name: Programming I, units: 4.00, description: A first course in computer science that uses a structured programming language to study programming and problem solving on the computer. Includes the design, construction and analysis of programs.  Student participation in a closed instructional lab is required.  This course will have additional fees.');
         expect(contentType).toStrictEqual('text/plain');
     });
 });
