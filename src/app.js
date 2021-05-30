c9a13d0c038bde52e1339ef2d413162b81be11be
Bruce Berrios <bberr022@fiu.edu>
2021-05-30 19:07:28 -0400

    Add /status endpoint

--- src/app.js
+++ src/app.js
@@ -5,14 +5,39 @@ const path = require('path');
 const { logger, errorHandler } = require('./middleware');
 const courseDataStore = require('./courseDataStore');
 const formatService = require('./formatService');
+const { version } = require('../package.json');
+
+const APP_START_TIME = new Date().getTime();
 
 app.use('/', express.static(path.resolve(__dirname, './public')));
 app.use(logger());
+app.disable('x-powered-by');
 
 app.get('/api', (req, res) => {
     const { query } = req;
     const results = courseDataStore.queryBy(query);
-    const { formattedData, contentType } = formatService.format(results, query.format);
+    const {
+        formattedData,
+        contentType
+    } = formatService.format(results, {
+        format: query.format,
+        ...courseDataStore.formatOptions,
+    });
+    res.setHeader('Content-Type', contentType);
+    return res.status(200).send(formattedData);
+});
+
+app.get('/status', (req, res) => {
+    const currentTime = new Date().getTime();
+    const statusData = {
+        version,
+        status: 'ok',
+        uptime: currentTime - APP_START_TIME,
+    };
+    const formatOptions = {
+        format: req.query.format,
+    };
+    const { formattedData, contentType } = formatService.format(statusData, formatOptions);
     res.setHeader('Content-Type', contentType);
     return res.status(200).send(formattedData);
 });
