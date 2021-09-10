b68d67db0877b3e67137da85c76bda17e53402dc
Bruce Berrios <bberr022@fiu.edu>
2021-09-10 17:01:39 -0400

    Log context

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,11 @@
 const logger = require('./logger');
+const getContextMiddleware = require('./context');
 const errorHandlers = require('./errorHandlers');
 const addSwaggerMiddleware = require('./swagger');
 
 module.exports = {
     logger,
+    getContextMiddleware,
     errorHandlers,
     addSwaggerMiddleware,
 }
